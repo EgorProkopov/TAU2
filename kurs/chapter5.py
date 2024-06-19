bacf26176bfab4a803d61b0bfb80b2db42c8e4ea
egor <egorprokopov216@gmai.com>
2024-06-20 00:24:57 +0300

    chapter 5 task 2 reports

--- kurs/chapter5.py
+++ kurs/chapter5.py
@@ -72,7 +72,7 @@ def task1(A, B, C, D):
 # task 2
 def task2(A, B, C, D):
     x0 = [1.0, 0, 0.0, 0.0]
-    time = set_time(10)
+    time = set_time(30)
     qs = [10.0, 1.0, 10.0, 1.0]
     rs = [1.0, 10.0, 0.1, 10.0]
 
