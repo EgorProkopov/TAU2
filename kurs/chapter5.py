46da64eeff13476ac0f2cfdb6f1564a3d4049f97
EgorProkopov <egorprokopov216@gmail.com>
2024-06-20 00:04:53 +0300

    kurs chapter 5 task 1 task 2

--- kurs/chapter5.py
+++ kurs/chapter5.py
@@ -14,14 +14,80 @@ from kurs.utils import *
 
 
 # task 1
+def get_k_lqr(A, B, Q, R):
+    k, _, _ = control.lqr(A, B, Q, R)
+    new_spec = np.linalg.eigvals(A + B @ k)
+    return k, new_spec
+
+
+def draw_nonlinear_lqr(k, x0, time, q, r, save_path):
+    ss_nonlin = control.NonlinearIOSystem(updfcn_lmi, params={"K": k})
+    ss_nonlin.set_inputs(2)
+
+    response_nonlin = control.input_output_response(ss_nonlin, T=time, X0=x0)
+
+    xs = response_nonlin.states
+    us = (- k @ xs).reshape(-1)
+
+    fig, ax = plt.subplots(4, figsize=(8, 12))
+    for i, state in enumerate(response_nonlin.states):
+        ax[i].plot(time, state, label=f'Q = ${q}I_Q$; R = ${r}I_R$')
+        ax[i].legend()
+        ax[i].set_xlabel('t')
+        ax[i].set_ylabel('$x_i$')
+        ax[i].grid()
+
+    fig_u, ax_u = plt.subplots(1, figsize=(8, 12))
+    ax_u[0].plot(time, us, label=f'Q = ${q}I_Q$; R = ${r}I_R$')
+    ax_u[0].legend()
+    ax_u[0].set_xlabel('t')
+    ax_u[0].set_ylabel('$x_i$')
+    ax_u[0].grid()
+
+    fig.savefig(f'{save_path}/task{q}_{r}_states.png')
+    fig_u.savefig(f'{save_path}/task5_{q}_{r}_us.png')
+
+
 def task1(A, B, C, D):
-    pass
+    x0 = [1.0, 0, 0.0, 0.0]
+    time = set_time(10)
+
+    q = 1.0
+    r = 1.0
+    Q = np.diag(np.ones((A.shape[0]))) * q
+    R = np.diag(np.ones((B.shape[1]))) * r
+
+    k, new_spec = get_k_lqr(A, B, Q, R)
+    k = -k
+    # контрол выдает отрицательное значение матрицы k
+
+    print(f"K: \n{k}")
+    print(f"new_spec: \n{new_spec}")
+
+    save_path = r"chapter5_reports/task1"
+    draw_nonlinear_lqr(k, x0, time, q, r, save_path)
 
 
 # ------------------------------------------
 # task 2
 def task2(A, B, C, D):
-    pass
+    x0 = [1.0, 0, 0.0, 0.0]
+    time = set_time(10)
+    qs = [1.0, 1.0, 10.0, 10.0]
+    rs = [1.0, 10.0, 0.1, 10.0]
+
+    for q, r in zip(qs, rs):
+        Q = np.diag(np.ones((A.shape[0]))) * q
+        R = np.diag(np.ones((B.shape[1]))) * r
+
+        k, new_spec = get_k_lqr(A, B, Q, R)
+        k = -k
+
+        print(f"K: \n{k}")
+        print(f"new_spec: \n{new_spec}")
+
+        save_path = r"chapter5_reports/task2"
+        draw_nonlinear_lqr(k, x0, time, q, r, save_path)
 
 
 # ------------------------------------------
